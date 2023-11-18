013aa8b336d6561faad829595fdcb68556685ab4
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2023-11-18 06:21:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,22 @@
 import streamlit as st
+from streamlit_chat import message
 import requests
-from datetime import datetime
 
 # Streamlit UI with Chat
 st.title('Virtual Psychologist and Dietitian Chat')
 
-# Initialize the conversation with the template
-initial_template = """[INST] <<SYS>> You are a virtual psychologist trained in Cognitive Behavioral Therapy (CBT) principles. \
-Your primary function is to guide users in identifying, understanding, and challenging their cognitive distortions and unhelpful beliefs. \
-Help users recognize the links between their thoughts, feelings, and behaviors. Encourage them to develop healthier thinking patterns and coping mechanisms. \
-You specialize in treating eating disorders such as bulimia, anorexia, orthorexia, and binge eating disorder. \
-You can help people with body dysmorphia reason through the emotions that are causing it through conversation, reflection worksheets, and homework tasks. \
-Remember to maintain empathy, confidentiality, and respect, but also note that you are not a substitute for professional, human-led therapy. \
-You know how to recommend macros, calories, and workouts to reach people's goals, but you also have compassion from the therapist's knowledge. \
-Whenever you are making a plan, you ask people for detailed information about their lifestyle, health, and habits so that you can make a plan that is sustainable and maintainable long term. \
-In all of your plans, you keep focus on protein intake and a healthy relationship with food using the 80/20 principle. \
-When recommending protein intake for an individual you suggest at least 1 gram per pound of body weight and make a calculation for them. \
-If you need details to make a calculation, you ask them for it.<SYS>> \
-Patient: {patient}[/INST]"""
+# Define the instruction template
+template = """[INST] <<SYS>> 
+        "You are a virtual psychologist trained in Cognitive Behavioral Therapy (CBT) principles. "
+        "Your primary function is to guide users in identifying, understanding, and challenging their cognitive distortions and unhelpful beliefs. "
+        "Help users recognize the links between their thoughts, feelings, and behaviors. Encourage them to develop healthier thinking patterns and coping mechanisms. "
+        "You specialize in treating eating disorders such as bulimia, anorexia, orthorexia, and binge eating disorder. You can help people with body dysmorphia reason through the emotions that are causing it through conversation, reflection worksheets, and homework tasks. "
+        "Remember to maintain empathy, confidentiality, and respect, but also note that you are not a substitute for professional, human-led therapy. "
+        "Always refer users to seek help from licensed professionals when necessary. Keep the answers short and simple. "
+        "You are also a certified personal trainer and a registered dietitian with in-depth knowledge on weight loss, muscle, and strength building. You know how to recommend macros, calories, and workouts to reach people's goals, but you also have compassion from the therapist's knowledge. Whenever you are making a plan, you ask people for detailed information about their lifestyle, health, and habits so that you can make a plan that is sustainable and maintainable long term. In all of your plans, you keep focus on protein intake and a healthy relationship with food using the 80/20 principle. "
+        "Keep each response below 100 words. "
+<<SYS>>
+Patient: {patient} [/INST]"""
 
 # Function to get response from the API
 def query_model(input_text):
@@ -34,12 +33,8 @@ def query_model(input_text):
     return response.json()
 
 def get_llm_response(user_input):
-    if 'initialized' not in st.session_state:
-        # Start the conversation with the initial template
-        st.session_state.initialized = True
-        query_model(initial_template)
-
-    response = query_model(user_input)
+    formatted_input = template.format(patient=user_input)
+    response = query_model(formatted_input)
     results = response.get('results', [])
     if results:
         return results[0].get('generated_text', '')
@@ -53,25 +48,12 @@ user_input = st.text_input("Enter your text here", key="input")
 
 if st.button("Send"):
     if user_input:
-        current_time = datetime.now().strftime("%H:%M")
-        st.session_state.chat_history.append({"message": user_input, "is_user": True, "timestamp": current_time})
+        st.session_state.chat_history.append({"message": user_input, "is_user": True})
         response = get_llm_response(user_input)
-        st.session_state.chat_history.append({"message": response, "is_user": False, "timestamp": current_time})
+        st.session_state.chat_history.append({"message": response, "is_user": False})
         # Clear the input box after sending the message
         st.experimental_rerun()
 
-# Display messages with unique keys and include timestamps
+# Display messages with unique keys
 for i, chat in enumerate(st.session_state.chat_history):
-    # We use a container to group each message with its timestamp
-    with st.container():
-        key = f"msg_{i}"
-        # Check if the message is from the user or from the system and assign a side
-        if chat["is_user"]:
-            st.text_area("", chat["message"], key=key, disabled=True)
-        else:
-            st.text_area("", chat["message"], key=key, disabled=True)
-        # Display the timestamp below the message
-        st.caption(chat["timestamp"])
-
-# Optional: Add a container or padding at the bottom if you want to ensure the last message is not covered by the input box
-st.container()
+    message(chat["message"], is_user=chat["is_user"], key=str(i))
