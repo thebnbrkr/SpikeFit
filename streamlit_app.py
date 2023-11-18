6648c6c2ceb156c19107c27aec6473254d29259c
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2023-11-18 06:10:53 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from streamlit_chat import message
 import requests
+from datetime import datetime
 
 # Streamlit UI with Chat
 st.title('Virtual Psychologist and Dietitian Chat')
 
-# Define the instruction template
-template = """[INST] <<SYS>> You are a virtual psychologist trained in Cognitive Behavioral Therapy (CBT) principles...
+# Initialize the conversation with the template
+initial_template = """[INST] <<SYS>> You are a virtual psychologist trained in Cognitive Behavioral Therapy (CBT) principles...
 [Your detailed instructions here]
-<<SYS>>
-Patient: {patient} [/INST]"""
+<<SYS>> [/INST]"""
 
 # Function to get response from the API
 def query_model(input_text):
@@ -26,8 +26,12 @@ def query_model(input_text):
     return response.json()
 
 def get_llm_response(user_input):
-    formatted_input = template.format(patient=user_input)
-    response = query_model(formatted_input)
+    if 'initialized' not in st.session_state:
+        # Start the conversation with the initial template
+        st.session_state.initialized = True
+        query_model(initial_template)
+
+    response = query_model(user_input)
     results = response.get('results', [])
     if results:
         return results[0].get('generated_text', '')
@@ -41,13 +45,17 @@ user_input = st.text_input("Enter your text here", key="input")
 
 if st.button("Send"):
     if user_input:
-        st.session_state.chat_history.append({"message": user_input, "is_user": True})
+        current_time = datetime.now().strftime("%H:%M")
+        st.session_state.chat_history.append({"message": user_input, "is_user": True, "timestamp": current_time})
         response = get_llm_response(user_input)
-        st.session_state.chat_history.append({"message": response, "is_user": False})
+        st.session_state.chat_history.append({"message": response, "is_user": False, "timestamp": current_time})
         # Clear the input box after sending the message
         st.experimental_rerun()
 
-# Display messages with unique keys
+# Display messages with unique keys and include timestamps
 for i, chat in enumerate(st.session_state.chat_history):
     message(chat["message"], is_user=chat["is_user"], key=str(i))
+    st.caption(chat["timestamp"])
 
+# Optional: Add a container or padding at the bottom if you want to ensure the last message is not covered by the input box
+st.container()
